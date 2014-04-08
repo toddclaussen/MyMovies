706de01a9eb8be81376f17cf225ea36d570d58d0
toddclaussen <s0468413@student.rockvalleycollege.edu>
2014-04-07 21:20:47 -0500

    MyMovies

---
+++ Trailer2ViewController.h
@@ -0,0 +1,18 @@
+//
+//  Trailer2ViewController.h
+//  MyMovies
+//
+//  Created by Todd Claussen on 4/7/14.
+//  Copyright (c) 2014 Todd Claussen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MediaPlayer/MediaPlayer.h>
+
+@interface Trailer2ViewController : UIViewController
+{
+    MPMoviePlayerController *player;
+}
+
+
+@end
---
+++ Trailer3ViewController.h
@@ -0,0 +1,18 @@
+//
+//  Trailer3ViewController.h
+//  MyMovies
+//
+//  Created by Todd Claussen on 4/7/14.
+//  Copyright (c) 2014 Todd Claussen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MediaPlayer/MediaPlayer.h>
+
+@interface Trailer3ViewController : UIViewController
+{
+    MPMoviePlayerController *player;
+}
+
+
+@end
